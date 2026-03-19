fc9a51688a88d3aac543c326bfea54e725dec1e1
Sid01-dev <sid01.dev@gmail.com>
2026-03-19 23:28:12 +0530

    Initial commit - Book My Stay App

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Book My Stay App
+ * Entry point of the Hotel Booking System
+ *
+ * @author Sid
+ * @version 1.0
+ */
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay App");
+        System.out.println("Hotel Booking System v1.0");
+    }
+}
